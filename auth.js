38c16720ea5a6864f41bfcc69100783753899f56
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-09 19:32:20 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,3 @@
+{
+   "token": "NDQzOTAyNjE0ODgzNzk0OTQ1.DdUICg.HynWumr_L-sRAuQkpgosLC_8llQ"
+}
---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'NA: Double lux active!') {
+     msg.channel.send('@everyone :fire: :lux: Lux Time!!! A darle con todo mis Keyblade Rangers~ :lux: :fire:', {files: ["https://cdn.discordapp.com/attachments/421507243318706188/442339823274033184/BONO_DE_LUX.png"]});
+  }
+});
+
+
+
+client.login('NDQzOTAyNjE0ODgzNzk0OTQ1.DdUICg.HynWumr_L-sRAuQkpgosLC_8llQ');
