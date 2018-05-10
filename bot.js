b75c23a444983d3525e9b79b829a590a0f740bee
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-10 15:05:13 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,12 +15,12 @@ client.on('message', msg => {
 
 client.on('message', function(message) {
     // Now, you can use the message variable inside
-    if (message.content === "$loop") { 
+    if (message.content === "$lo0op") { 
         var interval = setInterval (function () {
             // use the message's channel (TextChannel) to send a new message
             message.channel.send('@everyone :fire: :lux: Lux Time!!! A darle con todo mis Keyblade Rangers~ :lux: :fire:', {files: ["https://cdn.discordapp.com/attachments/421507243318706188/442339823274033184/BONO_DE_LUX.png"]})
             .catch(console.error); // add error handling here
-        }, 1 * 2160000); 
+        }, 1 * 21600000); 
     }
 });
 
