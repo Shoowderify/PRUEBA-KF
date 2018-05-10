0ec13b645a2363d4b15aa4d46e184f0d96dca293
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-10 14:17:58 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,15 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', message => {
+
+        if (message.channel.id === 275346465549975552)) {
+ if (msg.content === 'pong') {
+    msg.send('pongoooo!');
+        }
+
+});
+
 
 
 
