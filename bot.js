2846d5fe43a8a8cdd5e0eb1fb39ca51a0f8a58ee
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-11 17:12:07 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+await client.change_presence(game=discord.Game(name='KHUX'))
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
