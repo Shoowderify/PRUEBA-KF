8c2ba81776eebf7696e635a4055bdf6124bdb103
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-11 17:16:44 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => { bot.user.setGame('KHUX') })
+client.on('ready', () => { client.user.setGame('KHUX') })
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
